145e9083362feb5f7bb61d4cf3e73c2c23cabfbb
yasss <tripjitris@gmail.com>
2022-10-28 11:13:11 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const cors = require('cors');
+  app.use(cors());
+  await app.listen(3001);
 }
 bootstrap();
